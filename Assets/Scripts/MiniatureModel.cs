d78768d1e35ab72a17442da5d427c4316c360578
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-08-22 23:31:48 +0200

    Added player orientation

--- Assets/Scripts/MiniatureModel.cs
+++ Assets/Scripts/MiniatureModel.cs
@@ -11,13 +11,17 @@ public class MiniatureModel : MonoBehaviour {
     private Transform WIMLevelTransform;
     private Transform playerRepresentationTransform;
     private Transform playerTransform;
+    private Transform HMDTransform;
 
     void Awake() {
         levelTransform = GameObject.Find("Level").transform;
         WIMLevelTransform = GameObject.Find("WIM Level").transform;
         playerTransform = GameObject.Find("OVRCameraRig").transform;
+        HMDTransform = GameObject.Find("CenterEyeAnchor").transform;
         Assert.IsNotNull(levelTransform);
         Assert.IsNotNull(WIMLevelTransform);
+        Assert.IsNotNull(playerTransform);
+        Assert.IsNotNull(HMDTransform);
     }
 
     void Start() {
@@ -25,13 +29,35 @@ public class MiniatureModel : MonoBehaviour {
     }
 
     void Update() {
-        var playerOffset = playerTransform.position - levelTransform.position;
+        var eyeOffset = playerTransform.forward.normalized * -0.15f;
+        var playerOffset = (playerTransform.position + eyeOffset) - levelTransform.position;
         var playerRepresentationOffset = playerOffset * scaleFactor;
         playerRepresentationOffset = WIMLevelTransform.parent.rotation * playerRepresentationOffset;
-        playerRepresentationTransform.position = WIMLevelTransform.position + playerRepresentationOffset;
+        playerRepresentationTransform.position =
+        WIMLevelTransform.position + playerRepresentationOffset + new Vector3(0, 0.6f, 0) * scaleFactor;
+
+        //var playerRelativeRot = Quaternion.FromToRotation(levelTransform.rotation.eulerAngles, playerTransform.rotation.eulerAngles);
+        //playerRepresentationTransform.rotation = WIMLevelTransform.rotation * playerRelativeRot;
+        //Debug.Log(playerRepresentationTransform.rotation);
+
+        var lookAtPos = HMDTransform.position + HMDTransform.forward;
+        var lookAtOffset = lookAtPos - levelTransform.position;
+        var WIMLookAtOffset = lookAtOffset * scaleFactor;
+        WIMLookAtOffset = WIMLevelTransform.parent.rotation * WIMLookAtOffset;
+        var y = playerRepresentationTransform.rotation.y;
+        var lookPos = WIMLevelTransform.position + WIMLookAtOffset;
+        lookPos.y = 0;
+        playerRepresentationTransform.LookAt(lookPos);
+        playerRepresentationTransform.Rotate(-90, 0, 0);
+        playerRepresentationTransform.Rotate(0, -45, 0);
+
+
+        //playerRepresentationTransform.rotation = Quaternion.Euler(new Vector3(LeftEyeAnchorTransform.rotation.x,
+        //                                                                        y,
+        //                                                                        LeftEyeAnchorTransform.rotation.z));
 
         //playerRepresentationTransform.rotation = Quaternion.Euler(new Vector3(playerRepresentationTransform.rotation.x,
-        //                                                                        playerTransform.rotation.y,
+        //                                                                        LeftEyeAnchorTransform.rotation.y,
         //                                                                        playerRepresentationTransform.rotation.z));
     }
 }
