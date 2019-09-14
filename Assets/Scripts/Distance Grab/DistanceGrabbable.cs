7e4032b83eb351f3f1e8ca9b8891c9b90abafc17
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-09-14 02:20:27 +0200

    WIP distance grab

---
+++ Assets/Scripts/Distance Grab/DistanceGrabbable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceGrabbable : MonoBehaviour {
+    public Transform Target { get; set; }
+
+    public bool IsBeingGrabbed { get; set; }
+
+    public bool HightlightFX {
+        get => hightlightFX;
+        set {
+            hightlightFX = value;
+            GetComponent<Renderer>().material =
+                hightlightFX ? Resources.Load<Material>("Materials/outline") : defaultMaterial;
+        }
+    }
+
+    private bool hightlightFX;
+
+    public float SnapSpeed { get; set; } = 1f;
+    public float MinDistance { get; set; } = .1f;
+
+    private Material defaultMaterial;
+
+    void Awake() {
+        defaultMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+        HightlightFX = false;
+        if (!IsBeingGrabbed || !Target) return;
+
+        if (Vector3.Distance(Target.position, transform.position) < MinDistance) {
+            IsBeingGrabbed = false;
+            Target = null;
+        }
+        else {
+            //if (Physics.Raycast(transform.position, Target.position - transform.position, out var hit,
+            //    Mathf.Infinity)) {
+            //    if (hit.transform.tag != "Hands") {
+            //        IsBeingGrabbed = false;
+            //        Target = null;
+            //        return;
+            //    }
+            //}
+
+            var step = SnapSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+        }
+    }
+}
---
+++ Assets/Scripts/Distance Grab/DistanceGrabber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceGrabber : MonoBehaviour {
+    [SerializeField] private OVRInput.RawButton grabButton;
+    [SerializeField] private float snapSpeed = 10f;
+    [SerializeField] private float minDistance = .5f;
+
+
+    void LateUpdate() {
+        var WIMLayerOnly = 1 << 8;
+        if (Physics.Raycast(transform.position, transform.GetChild(0).forward, out var hit,
+            Mathf.Infinity, WIMLayerOnly)) {
+            var grabbable = hit.transform.gameObject.GetComponent<DistanceGrabbable>();
+            if (!grabbable) return;
+            grabbable.HightlightFX = true;
+            if (OVRInput.GetDown(grabButton) || Input.GetKeyDown(KeyCode.Y)) {
+                grabbable.MinDistance = minDistance;
+                grabbable.SnapSpeed = snapSpeed;
+                grabbable.Target = transform;
+                grabbable.IsBeingGrabbed = true;
+            };
+        }
+    }
+}
