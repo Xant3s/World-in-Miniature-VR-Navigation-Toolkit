9abb75d609bfbc50c15f049173a138a64f604459
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-09-14 03:38:33 +0200

    WIP improved distance grab

---
+++ Assets/Scripts/Distance Grab/AimAssist.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class AimAssist : MonoBehaviour {
+
+    [SerializeField] private OVRInput.RawButton grabButton;
+    [SerializeField] private float length = 1.0f;
+
+    void Update() {
+        var lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, transform.position + transform.forward * length);
+        lr.enabled = !OVRInput.Get(grabButton);
+    }
+}
