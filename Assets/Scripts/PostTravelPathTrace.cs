6272ec1a4a401f46f66495e48ad6722668919fc0
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-09-15 12:27:07 +0200

    WIP visualization

--- Assets/Scripts/PostTravelPathTrace.cs
+++ Assets/Scripts/PostTravelPathTrace.cs
@@ -25,15 +25,34 @@ public class PostTravelPathTrace : MonoBehaviour {
         oldPositionInWIM = WIMLevelTransform.Find("PathTraceOldPosition").transform;
         newPositionInWIM = WIMLevelTransform.Find("PathTraceNewPosition").transform;
         lr.widthMultiplier = .001f;
-        lr.material = Resources.Load<Material>("Materials/Blue");
+        //lr.material = Resources.Load<Material>("Materials/Blue");
+        lr.material = new Material(Shader.Find("Sprites/Default"));
         endTime = Time.time + TraceDurationInSeconds;
     }
 
     void Update() {
+        if (Time.time >= endTime) {
+            Destroy(gameObject);
+        }
+
         lr.SetPosition(0, oldPositionInWIM.position);
         lr.SetPosition(1, newPositionInWIM.position);
 
+        var timeLeft = (endTime - Time.time);
+        var timePast = TraceDurationInSeconds - timeLeft;
+        var progress = timePast / TraceDurationInSeconds;
+        progress = Mathf.Clamp(progress, 0, 1);
 
+        var gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.green, 0), new GradientColorKey(Color.red, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1-progress, progress), new GradientAlphaKey(1, 1.0f) }
+        );
+        //gradient.SetKeys(
+        //    new GradientColorKey[]{new GradientColorKey(new Color(0,1,0,1), 0), new GradientColorKey(new Color(1,0,0,1), 1)}, 
+        //    new GradientColorKey[]{new GradientColorKey(new Color(0,1,0,1), 0), new GradientColorKey(new Color(1,0,0,1), 1)} 
+        //    );
+        lr.colorGradient = gradient;
     }
 
     void OnDestroy() {
---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    void Start() {
+        var lr = GetComponent<LineRenderer>();
+        var gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] {new GradientColorKey(Color.green, 0), new GradientColorKey(Color.red, 1.0f)},
+            new GradientAlphaKey[] {new GradientAlphaKey(.5f, 0), new GradientAlphaKey(1, 1.0f)}
+        );
+        lr.colorGradient = gradient;
+    }
+}
