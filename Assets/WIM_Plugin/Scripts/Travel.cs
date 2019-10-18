3d00d19e0e84d4d95b0e986223f30b1a4cad295c
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-10-18 17:36:52 +0200

    .

--- Assets/WIM_Plugin/Scripts/Travel.cs
+++ Assets/WIM_Plugin/Scripts/Travel.cs
@@ -12,14 +12,11 @@ namespace WIM_Plugin {
         public void Travel(MiniatureModel WIM) {
             WIM.Data.WIMLevelLocalPosOnTravel = WIM.transform.GetChild(0).localPosition;
             WIM.transform.parent = WIM.Data.OVRPlayerController; // Maintain transform relative to player.
-            WIM.Data.WIMHeightRelativeToPlayer =
-                WIM.transform.position.y -
-                WIM.Data.OVRPlayerController.position.y; // Maintain height relative to player.
-            var playerHeight = WIM.Data.OVRPlayerController.position.y -
-                               MathUtils.GetGroundPosition(WIM.Data.OVRPlayerController.position).y;
-            WIM.Data.OVRPlayerController.position =
-                MathUtils.GetGroundPosition(WIM.Data.DestinationIndicatorInLevel.position) + Vector3.up * playerHeight;
+            WIM.Data.WIMHeightRelativeToPlayer = WIM.transform.position.y - WIM.Data.OVRPlayerController.position.y; // Maintain height relative to player.
+            var playerHeight = WIM.Data.OVRPlayerController.position.y - MathUtils.GetGroundPosition(WIM.Data.OVRPlayerController.position).y;
+            WIM.Data.OVRPlayerController.position = MathUtils.GetGroundPosition(WIM.Data.DestinationIndicatorInLevel.position) + Vector3.up * playerHeight;
             WIM.Data.OVRPlayerController.rotation = WIM.Data.DestinationIndicatorInLevel.rotation;
+            WIM.transform.parent = null;
             WIM.GetComponent<Respawn>().respawnWIM(true); // Assist player to orientate at new location.
         }
     }
