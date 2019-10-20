51068b2285c4ba3e8edbf4dad9f33a765e9380d0
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-10-20 22:55:14 +0200

    Fixed player not always teleported to floor if snap is disabled

--- Assets/WIM_Plugin/Scripts/Travel.cs
+++ Assets/WIM_Plugin/Scripts/Travel.cs
@@ -14,7 +14,9 @@ namespace WIM_Plugin {
             WIM.transform.parent = WIM.Data.OVRPlayerController; // Maintain transform relative to player.
             WIM.Data.WIMHeightRelativeToPlayer = WIM.transform.position.y - WIM.Data.OVRPlayerController.position.y; // Maintain height relative to player.
             var playerHeight = WIM.Data.OVRPlayerController.position.y - MathUtils.GetGroundPosition(WIM.Data.OVRPlayerController.position).y;
-            WIM.Data.OVRPlayerController.position = MathUtils.GetGroundPosition(WIM.Data.DestinationIndicatorInLevel.position) + Vector3.up * playerHeight;
+            WIM.Data.OVRPlayerController.position = WIM.Configuration.DestinationAlwaysOnTheGround
+                ? MathUtils.GetGroundPosition(WIM.Data.DestinationIndicatorInLevel.position) + Vector3.up * playerHeight
+                : WIM.Data.DestinationIndicatorInLevel.position;
             WIM.Data.OVRPlayerController.rotation = WIM.Data.DestinationIndicatorInLevel.rotation;
             WIM.transform.parent = null;
             WIM.GetComponent<Respawn>().respawnWIM(true); // Assist player to orientate at new location.
