60e74a3656680fc2ef51246c143fce126063123c
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-01-09 02:19:42 +0100

    Added welcome window (no functionality yet)

--- Assets/WIM_Plugin/Editor/Welcome/WelcomeWindow.cs
+++ Assets/WIM_Plugin/Editor/Welcome/WelcomeWindow.cs
@@ -6,6 +6,11 @@ using UnityEngine.UIElements;
 
 namespace WIM_Plugin {
     public class WelcomeWindow : EditorWindow {
+        private static readonly bool isProVersion = false;
+        private static readonly string pluginVersion = "0.9.0";
+        private static readonly string imagePath = "Assets/WIM_Plugin/Sprites/";
+
+
         [MenuItem("Window/WIM Plugin/ Welcome Window")]
         public static void ShowWindow() {
             var window = GetWindow<WelcomeWindow>();
@@ -19,6 +24,13 @@ namespace WIM_Plugin {
             VisualElement uxmlContents = visualTree.CloneTree();
             uxmlContents.styleSheets.Add(styleSheet);
             root.Add(uxmlContents);
+
+            root.Q<Image>(name: "SceneIcon").image = EditorGUIUtility.IconContent("SceneAsset Icon").image;
+            root.Q<Image>(name: "YoutubeIcon").image = AssetDatabase.LoadAssetAtPath<Texture>(imagePath + "youtube.png");
+            root.Q<Image>(name: "ManualIcon").image = EditorGUIUtility.IconContent("_Help").image;
+            root.Q<Image>(name: "EmailIcon").image = AssetDatabase.LoadAssetAtPath<Texture>(imagePath + "email.png");
+
+            root.Q<Label>(name: "VersionNumber").text = pluginVersion;
 
             root.Q<Button>("ExampleSceneBtn").RegisterCallback<MouseUpEvent>((e) => {
                 Debug.Log("Example Scene Button");
@@ -34,6 +46,11 @@ namespace WIM_Plugin {
 
             root.Q<Button>("SupportEmailBtn").RegisterCallback<MouseUpEvent>((e) => {
                 Debug.Log("Support Email Button");
+            });
+
+            root.Q<VisualElement>(name: "FullFeatureLabel").visible = !isProVersion;
+            root.Q<Label>(name: "FullFeatureURL").RegisterCallback<MouseUpEvent>((e) => {
+                Debug.Log("URL to full feature version");
             });
 
             var showOnStartupToggle = root.Q<Toggle>("showOnStartup");
