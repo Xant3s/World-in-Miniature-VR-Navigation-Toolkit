288d7cc9f35fe65e0faf837503d76cc7603e21f7
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-03-12 19:33:51 +0100

    WIP convert to UIElements

--- Assets/WIM_Plugin/Editor/Core/MiniatureModelEditor.cs
+++ Assets/WIM_Plugin/Editor/Core/MiniatureModelEditor.cs
@@ -33,6 +33,7 @@ namespace WIM_Plugin {
         }
 
         public override VisualElement CreateInspectorGUI() {
+            if(!visualTree) return new VisualElement();
             root.Q<ObjectField>("configuration").objectType = typeof(WIMConfiguration);     // Hotfix until 2020.1
             //configField.RegisterCallback<ChangeEvent<UnityEngine.Object>>((e) => {
             //    Debug.Log("update");
@@ -40,8 +41,8 @@ namespace WIM_Plugin {
             //    //root.Q<VisualElement>("master-container").style.display = WIM.Configuration ? DisplayStyle.Flex : DisplayStyle.None;
             //});
 
-            root.Q<HelpBox>(name: "config-missing").style.display = !WIM.Configuration ? DisplayStyle.Flex : DisplayStyle.None;
-            root.Q<VisualElement>("master-container").style.display = WIM.Configuration ? DisplayStyle.Flex : DisplayStyle.None;
+            root.Q<HelpBox>(name: "config-missing").SetDisplay(!WIM.Configuration);
+            root.Q<VisualElement>("master-container").SetDisplay(WIM.Configuration);
 
             root.Q<Button>("GenerateWIMButton").RegisterCallback<MouseUpEvent>(e => {
                 WIMGenerator.GenerateNewWIM(WIM);
@@ -51,14 +52,14 @@ namespace WIM_Plugin {
             root.Q<ObjectField>("destination-indicator").objectType = typeof(GameObject);   // Hotfix until 2020.1
 
             var scaleFactorInputField = root.Q<FloatField>("scale-factor-input-field");
-            var scaleFactor = root.Q<Slider>("scale-factor");
+            var scaleFactorSlider = root.Q<Slider>("scale-factor");
             scaleFactorInputField.value = WIM.Configuration.ScaleFactor;
-            scaleFactor.RegisterValueChangedCallback(e => {
+            scaleFactorSlider.RegisterValueChangedCallback(e => {
                 scaleFactorInputField.SetValueWithoutNotify(e.newValue);
             });
             scaleFactorInputField.RegisterValueChangedCallback(e => {
                 var newValue = Mathf.Clamp(scaleFactorInputField.value, 0f, 1f);
-                scaleFactor.value = newValue;
+                scaleFactorSlider.value = newValue;
                 scaleFactorInputField.SetValueWithoutNotify(newValue);
             });
 
@@ -75,10 +76,46 @@ namespace WIM_Plugin {
                 InvokeCallbacks("Basic");
             }));
 
-
             var destinationSelectionTouchAvailable = WIM.GetComponent<DestinationSelectionTouch>() != null;
             root.Q<EnumField>("destination-selection-method").SetEnabled(destinationSelectionTouchAvailable);
-            root.Q<HelpBox>("destination-selection-method-info").style.display = !destinationSelectionTouchAvailable ? DisplayStyle.Flex : DisplayStyle.None;
+            root.Q<HelpBox>("destination-selection-method-info").SetDisplay(!destinationSelectionTouchAvailable);
+            root.Q<FloatField>("double-tap-interval").SetDisplay(WIM.Configuration.DestinationSelectionMethod == DestinationSelection.Pickup);
+
+            root.Q<VisualElement>("input-container").Add(new IMGUIContainer(() => {
+                InvokeCallbacks("Input");
+            }));
+
+            root.Q<Toggle>("semi-transparent").RegisterValueChangedCallback(e => WIMGenerator.ConfigureWIM(WIM));
+
+            var transparencySlider = root.Q<Slider>("transparency");
+            transparencySlider.RegisterValueChangedCallback(e => WIMGenerator.ConfigureWIM(WIM));
+            transparencySlider.SetDisplay(WIM.Configuration.SemiTransparent);
+            var transparencyInputField = root.Q<FloatField>("transparency-input-field");
+            transparencyInputField.value = WIM.Configuration.Transparency;
+            transparencySlider.RegisterValueChangedCallback(e => {
+                transparencyInputField.SetValueWithoutNotify(e.newValue);
+            });
+            transparencyInputField.RegisterValueChangedCallback(e => {
+                var newValue = Mathf.Clamp(transparencyInputField.value, 0f, 1f);
+                transparencySlider.value = newValue;
+                transparencyInputField.SetValueWithoutNotify(newValue);
+            });
+
+            root.Q<VisualElement>("occlusion-handling-container").Add(new IMGUIContainer(() => {
+                InvokeCallbacks("Occlusion Handling");
+            }));
+
+            root.Q<FloatField>("spawn-distance").SetDisplay(!WIM.Configuration.AutoDetectArmLength);
+
+            var autoDetectArmLengthAvailable = WIM.GetComponent<DetectArmLength>() != null;
+            var detectArmLengthToggle = root.Q<Toggle>("detect-arm-length");
+            detectArmLengthToggle.SetEnabled(autoDetectArmLengthAvailable);
+            root.Q<HelpBox>("detect-arm-length-info").SetDisplay(!autoDetectArmLengthAvailable);
+            if(!autoDetectArmLengthAvailable)WIM.Configuration.AutoDetectArmLength = false;
+
+            root.Q<VisualElement>("usability-container").Add(new IMGUIContainer(() => {
+                InvokeCallbacks("Usability");
+            }));
 
 
             Action action = () => {
@@ -87,102 +124,9 @@ namespace WIM_Plugin {
                     fontStyle = FontStyle.Bold
                 };
 
-
                 if(!WIM.Configuration) {
                     return;
                 }
-
-                //Separator("Basic Settings");
-                //WIM.Configuration.PlayerRepresentation = (GameObject) EditorGUILayout.ObjectField(
-                //    "Player Representation",
-                //    WIM.Configuration.PlayerRepresentation, typeof(GameObject), false);
-                //WIM.Configuration.DestinationIndicator = (GameObject) EditorGUILayout.ObjectField(
-                //    "Destination Indicator",
-                //    WIM.Configuration.DestinationIndicator, typeof(GameObject), false);
-                //WIM.Configuration.ScaleFactor =
-                //    EditorGUILayout.Slider("Scale Factor", WIM.Configuration.ScaleFactor, 0, 1);
-                //WIM.Configuration.WIMLevelOffset =
-                //    EditorGUILayout.Vector3Field("WIM Level Offset", WIM.Configuration.WIMLevelOffset);
-                //WIM.Configuration.ExpandCollidersX =
-                //    WIMEditorUtility.NamedVectorField("Expand Colliders X", WIM.Configuration.ExpandCollidersX, "Left",
-                //        "Right");
-                //WIM.Configuration.ExpandCollidersY =
-                //    WIMEditorUtility.NamedVectorField("Expand Colliders Y", WIM.Configuration.ExpandCollidersY, "Up",
-                //        "Down");
-                //WIM.Configuration.ExpandCollidersZ =
-                //    WIMEditorUtility.NamedVectorField("Expand Colliders Z", WIM.Configuration.ExpandCollidersZ, "Front",
-                //        "Back");
-                //WIM.Configuration.DestinationAlwaysOnTheGround = EditorGUILayout.Toggle(
-                //    new GUIContent("Destination Always on the Ground",
-                //        "If active, the destination will automatically set to ground level. This protects the player from being teleported to a location in mid-air."),
-                //    WIM.Configuration.DestinationAlwaysOnTheGround);
-                //InvokeCallbacks("Basic");
-
-                //var destinationSelectionTouchAvailable = WIM.GetComponent<DestinationSelectionTouch>() != null;
-                //EditorGUI.BeginDisabledGroup(!destinationSelectionTouchAvailable);
-                //WIM.Configuration.DestinationSelectionMethod =
-                //    (DestinationSelection) EditorGUILayout.EnumPopup("Destination Selection Method",
-                //        WIM.Configuration.DestinationSelectionMethod);
-                //EditorGUI.EndDisabledGroup();
-                //if(!destinationSelectionTouchAvailable) {
-                //    WIM.Configuration.DestinationSelectionMethod = DestinationSelection.Pickup;
-                //    EditorGUILayout.HelpBox(
-                //        "Add 'DestinationSelectionTouch' script to change destination selection method",
-                //        MessageType.Info);
-                //}
-
-                if(WIM.Configuration.DestinationSelectionMethod == DestinationSelection.Pickup) {
-                    WIM.Configuration.DoubleTapInterval =
-                        EditorGUILayout.FloatField("Double Tap Interval", WIM.Configuration.DoubleTapInterval);
-                }
-
-                InvokeCallbacks("Input");
-
-
-                Separator("Occlusion Handling");
-                EditorGUI.BeginChangeCheck();
-                WIM.Configuration.SemiTransparent =
-                    EditorGUILayout.Toggle("Semi-Transparent", WIM.Configuration.SemiTransparent);
-                if(EditorGUI.EndChangeCheck()) WIMGenerator.ConfigureWIM(WIM);
-                if(WIM.Configuration.SemiTransparent) {
-                    EditorGUI.BeginChangeCheck();
-                    WIM.Configuration.Transparency =
-                        EditorGUILayout.Slider("Transparency", WIM.Configuration.Transparency, 0, 1);
-                    if(EditorGUI.EndChangeCheck()) WIMGenerator.ConfigureWIM(WIM);
-                }
-
-                InvokeCallbacks("Occlusion Handling");
-
-
-                Separator("Usability");
-                WIM.Configuration.WIMSpawnHeight =
-                    EditorGUILayout.FloatField("WIM Spawn at Height", WIM.Configuration.WIMSpawnHeight);
-                WIM.Configuration.PlayerHeightInCM =
-                    EditorGUILayout.FloatField("Player Height (in cm)", WIM.Configuration.PlayerHeightInCM);
-                if(!WIM.Configuration.AutoDetectArmLength) {
-                    WIM.Configuration.SpawnDistance =
-                        EditorGUILayout.FloatField("WIM Spawn Distance", WIM.Configuration.SpawnDistance);
-                }
-
-                var autoDetectArmLengthAvailable = WIM.GetComponent<DetectArmLength>() != null;
-                EditorGUI.BeginDisabledGroup(!autoDetectArmLengthAvailable);
-                WIM.Configuration.AutoDetectArmLength = EditorGUILayout.Toggle(
-                    new GUIContent("Auto Detect Arm Length",
-                        "At the start of the application, player has to extend the arm and press the confirm teleport button." +
-                        "The detected arm length will be used instead of the spawn distance."),
-                    WIM.Configuration.AutoDetectArmLength);
-                if(!autoDetectArmLengthAvailable) {
-                    WIM.Configuration.AutoDetectArmLength = false;
-                    EditorGUILayout.HelpBox("Add 'DetectArmLength' script to enable this feature.'", MessageType.Info);
-                }
-
-                EditorGUI.EndDisabledGroup();
-
-
-                WIM.Configuration.AdaptWIMSizeToPlayerHeight = EditorGUILayout.Toggle("Adapt WIM Size to Player Height",
-                    WIM.Configuration.AdaptWIMSizeToPlayerHeight);
-                InvokeCallbacks("Usability");
-
 
                 separators.Clear();
                 EditorUtility.SetDirty(WIM.Configuration);
@@ -213,6 +157,17 @@ namespace WIM_Plugin {
             if(separators.Contains(text)) return;
             Separator(text, space);
             separators.Add(text);
+        }
+    }
+
+    public static class VEExtension {
+        /// <summary>
+        /// Changes the VisualElement display state. If element is not displayed, it is invisible and doesn't use any space.
+        /// </summary>
+        /// <param name="element">The element to change.</param>
+        /// <param name="value">Whether to display element.</param>
+        public static void SetDisplay(this VisualElement element, bool value) {
+            element.style.display = value ? DisplayStyle.Flex : DisplayStyle.None;
         }
     }
 }
