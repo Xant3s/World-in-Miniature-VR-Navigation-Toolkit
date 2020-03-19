b45f5c1b954a3c373029b505d1fac01ff5719f0e
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-03-19 03:07:19 +0100

    Bugfix: scrolling cutout is now preserved on respawn WIM

--- Assets/WIM_Plugin/Scripts/Core/Travel.cs
+++ Assets/WIM_Plugin/Scripts/Core/Travel.cs
@@ -10,7 +10,6 @@ namespace WIM_Plugin {
 
     public class InstantTravel : TravelStrategy {
         public void Travel(MiniatureModel WIM) {
-            WIM.Data.WIMLevelLocalPosOnTravel = WIM.transform.GetChild(0).localPosition;
             WIM.transform.parent = WIM.Data.OVRPlayerController; // Maintain transform relative to player.
             WIM.Data.WIMHeightRelativeToPlayer = WIM.transform.position.y - WIM.Data.OVRPlayerController.position.y; // Maintain height relative to player.
             var playerHeight = WIM.Data.OVRPlayerController.position.y - MathUtils.GetGroundPosition(WIM.Data.OVRPlayerController.position).y;
