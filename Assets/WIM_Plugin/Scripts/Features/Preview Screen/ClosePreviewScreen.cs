932aa6447574b5ed6be8179fa2e33e035977c9cb
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-03-21 03:26:01 +0100

    Improved: preview screen can now be also closed using left index

--- Assets/WIM_Plugin/Scripts/Features/Preview Screen/ClosePreviewScreen.cs
+++ Assets/WIM_Plugin/Scripts/Features/Preview Screen/ClosePreviewScreen.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -10,13 +9,10 @@ namespace WIM_Plugin {
     [DisallowMultipleComponent]
     public class ClosePreviewScreen : MonoBehaviour {
         private MiniatureModel WIM;
-        private Transform index;
 
 
         private void Awake() {
-            index = GameObject.FindWithTag("IndexR")?.transform;
             WIM = GameObject.FindWithTag("WIM")?.GetComponent<MiniatureModel>();
-            Assert.IsNotNull(index);
             Assert.IsNotNull(WIM);
         }
 
@@ -26,20 +22,21 @@ namespace WIM_Plugin {
 
         private bool once;
         private void OnTriggerEnter(Collider other) {
-            if (other.transform != index) return;
-            if (transform.root.CompareTag("HandR")) return;
+            if(!other.CompareTag("IndexR") && !other.CompareTag("IndexL")) return;
             if (once) return;
             once = true;
-            startClosing();
+            var hand = other.CompareTag("IndexR") ? Hand.RightHand : Hand.LeftHand;
+            startClosing(hand);
         }
 
-        private void startClosing() {
-            InputManager.SetVibration(frequency: .5f, amplitude: .1f, Hand.RightHand);
-            Invoke(nameof(close), time: .1f);
+        private void startClosing(Hand hand) {
+            InputManager.SetVibration(frequency: .5f, amplitude: .1f, hand);
+            StartCoroutine(Close(hand, .1f));
         }
 
-        private void close() {
-            InputManager.SetVibration(frequency: 0, amplitude: 0, Hand.RightHand);
+        private IEnumerator Close(Hand hand, float time) {
+            yield return new WaitForSeconds(time);
+            InputManager.SetVibration(frequency: 0, amplitude: 0, hand);
             WIM.GetComponent<PreviewScreen>().RemovePreviewScreen();
         }
     }
