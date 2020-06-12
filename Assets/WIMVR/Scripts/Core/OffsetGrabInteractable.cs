8d25ed45c94f97c8fc110a2207740122474b38b7
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-06-12 03:16:25 +0200

    Fixed WIM drifting off after grabbing

--- Assets/WIMVR/Scripts/Core/OffsetGrabInteractable.cs
+++ Assets/WIMVR/Scripts/Core/OffsetGrabInteractable.cs
@@ -6,11 +6,16 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 namespace WIMVR.Core {
     public class OffsetGrabInteractable : XRGrabInteractable {
+        [SerializeField] private bool restoreRigidbodyOnRelease;
+
         private Vector3 interactorPosition;
         private Quaternion interactorRotation;
+        private float drag;
+        private float angularDrag;
 
 
         protected override void OnSelectEnter(XRBaseInteractor interactor) {
+            SaveRigidbody();
             base.OnSelectEnter(interactor);
             interactorPosition = interactor.attachTransform.localPosition;
             interactorRotation = interactor.attachTransform.localRotation;
@@ -24,6 +29,25 @@ namespace WIMVR.Core {
             interactor.attachTransform.localRotation = interactorRotation;
             interactorPosition = Vector3.zero;
             interactorRotation = Quaternion.identity;
+            RestoreRigidbody();
+        }
+
+        private void SaveRigidbody() {
+            var rb = GetComponent<Rigidbody>();
+            if(restoreRigidbodyOnRelease && rb) {
+                drag = rb.drag;
+                angularDrag = rb.angularDrag;
+            }
+        }
+
+        private void RestoreRigidbody() {
+            var rb = GetComponent<Rigidbody>();
+            if(rb && restoreRigidbodyOnRelease) {
+                rb.angularVelocity = Vector3.zero;
+                rb.velocity = Vector3.zero;
+                rb.drag = drag;
+                rb.angularDrag = angularDrag;
+            }
         }
     }
 }
