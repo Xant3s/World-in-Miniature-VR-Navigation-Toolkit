60235e0a5cafc74181c4e68dee06c829d4f38860
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2020-08-08 22:44:18 +0200

    Fixed scaling

--- Assets/WIMVR/Scripts/Features/Scaling/Scaling.cs
+++ Assets/WIMVR/Scripts/Features/Scaling/Scaling.cs
@@ -14,19 +14,17 @@ namespace WIMVR.Features.Scaling {
     /// <summary>
     /// Allows to scale the miniature model at runtime.
     /// </summary>
-    // [ExecuteAlways]
     [DisallowMultipleComponent]
     [RequireComponent(typeof(OffsetGrabInteractable))]
     public class Scaling : MonoBehaviour {
         public ScalingConfiguration ScalingConfig;
         private WIMConfiguration config;
-        private WIMData data;
         private OffsetGrabInteractable grabbable;
         private Transform handL;
         private Transform handR;
         private Transform WIMTransform;
-        private CapsuleCollider leftGrabVolume;
-        private CapsuleCollider rightGrabVolume = null;
+        private SphereCollider leftGrabVolume;
+        private SphereCollider rightGrabVolume;
         private Hand scalingHand = Hand.None;
         private InputHelpers.Button leftGrabButton = InputHelpers.Button.Grip;
         private InputHelpers.Button rightGrabButton = InputHelpers.Button.Grip;
@@ -39,31 +37,17 @@ namespace WIMVR.Features.Scaling {
             if(!ScalingConfig) return;
             MiniatureModel.OnUpdate += ScaleWIM;
             DetectGrabButtons();
-            //MiniatureModel.OnLeftGrabButtonDown += LeftScalingButtonDown;
-            //MiniatureModel.OnLeftGrabButtonUp += LeftScalingButtonUp;
-            //MiniatureModel.OnRightGrabButtonDown += RightScalingButtonDown;
-            //MiniatureModel.OnRightGrabButtonUp += RightScalingButtonUp;
-            //WIMGenerator.OnAdaptScaleToPlayerHeight += ClampScaleFactor;
         }
 
         private void OnDisable() {
             if(!ScalingConfig) return;
             MiniatureModel.OnUpdate -= ScaleWIM;
-            //MiniatureModel.OnLeftGrabButtonDown -= LeftScalingButtonDown;
-            //MiniatureModel.OnLeftGrabButtonUp -= LeftScalingButtonUp;
-            //MiniatureModel.OnRightGrabButtonDown -= RightScalingButtonDown;
-            //MiniatureModel.OnRightGrabButtonUp -= RightScalingButtonUp;
-            //WIMGenerator.OnAdaptScaleToPlayerHeight -= ClampScaleFactor;
         }
 
         private void Awake() {
             if(!ScalingConfig) return;
             WIMTransform = GameObject.FindWithTag("WIM").transform;
             grabbable = WIMTransform.GetComponent<OffsetGrabInteractable>();
-            // Assert.IsNotNull(leftGrabVolume);
-            // Assert.IsNotNull(rightGrabVolume);
-            // Assert.IsNotNull(handL);
-            // Assert.IsNotNull(handR);
         }
 
         private void Start() {
@@ -79,11 +63,11 @@ namespace WIMVR.Features.Scaling {
             var leftInputDevice = XRUtils.FindCorrespondingInputDevice(Hand.LeftHand);
             var rightInputDevice = XRUtils.FindCorrespondingInputDevice(Hand.RightHand);
             leftInputDevice.TryGetFeatureValue(new InputFeatureUsage<float>(leftGrabButton.ToString()), out var leftGrabValue);
-            rightInputDevice.TryGetFeatureValue(new InputFeatureUsage<float>(rightInputDevice.ToString()),
+            rightInputDevice.TryGetFeatureValue(new InputFeatureUsage<float>(rightGrabButton.ToString()),
                 out var rightGrabValue);
             const float epsilon = .01f;
-            var leftGrabPressed = leftGrabValue < epsilon;
-            var rightGrabPressed = rightGrabValue < epsilon;
+            var leftGrabPressed = leftGrabValue > epsilon;
+            var rightGrabPressed = rightGrabValue > epsilon;
 
             // call events
             if (leftGrabPressed && !leftGrabPressedLastFrame) SetScalingHand(Hand.LeftHand);
@@ -100,25 +84,9 @@ namespace WIMVR.Features.Scaling {
         private void Init() {
             handL = GameObject.FindWithTag("HandL")?.transform;
             handR = GameObject.FindWithTag("HandR")?.transform;
-            if(handL) leftGrabVolume = handL.GetComponentInChildren<CapsuleCollider>();
-            if(handR) rightGrabVolume = handR.GetComponentInChildren<CapsuleCollider>();
+            if(handL) leftGrabVolume = handL.GetComponentInParent<SphereCollider>();
+            if(handR) rightGrabVolume = handR.GetComponentInParent<SphereCollider>();
         }
-
-        // private void LeftScalingButtonDown(WIMConfiguration config, WIMData data) {
-        //     SetScalingHand(Hand.LeftHand);
-        // }
-        //
-        // private void LeftScalingButtonUp(WIMConfiguration config, WIMData data) {
-        //     scalingHand = Hand.None;
-        // }
-        //
-        // private void RightScalingButtonDown(WIMConfiguration config, WIMData data) {
-        //     SetScalingHand(Hand.RightHand);
-        // }
-        //
-        // private void RightScalingButtonUp(WIMConfiguration config, WIMData data) {
-        //     scalingHand = Hand.None;
-        // }
 
         private void SetScalingHand(Hand hand) {
             // Only if WIM scaling is enabled and WIM is currently being grabbed with one hand.
@@ -137,8 +105,7 @@ namespace WIMVR.Features.Scaling {
         }
 
         private void ScaleWIM(WIMConfiguration configuration, WIMData data) {
-            this.config = configuration;
-            this.data = data;
+            config = configuration;
             Assert.IsNotNull(ScalingConfig, "Scaling configuration is missing.");
 
             // Only if WIM scaling is enabled and WIM is currently being grabbed with one hand.
@@ -146,7 +113,6 @@ namespace WIMVR.Features.Scaling {
 
             // Check if currently scaling. Abort if not.
             if (scalingHand == Hand.None) return;
-            Debug.Log("asdf");
 
             // Scale using inter hand distance delta.
             var currInterHandDistance = Vector3.Distance(handL.position, handR.position);
@@ -176,49 +142,15 @@ namespace WIMVR.Features.Scaling {
         private bool GetHandIsInside(Hand hand) {
             if (hand == Hand.None) return false;
             var grabVolume = hand == Hand.LeftHand ? leftGrabVolume : rightGrabVolume;
-            GetWorldSpaceCapsule(grabVolume, out var p1, out var p2, out var radius);
-            var hitColliders = Physics.OverlapCapsule(p1, p2, radius, LayerMask.GetMask("WIM"));
+            GetWorldSpaceSphere(grabVolume, out var center, out var radius);
+            var hitColliders = Physics.OverlapSphere(center, radius, LayerMask.GetMask("WIM"));
             return hitColliders.Length != 0;
         }
-        
-        private static void GetWorldSpaceCapsule(CapsuleCollider capsule, out Vector3 point0, out Vector3 point1, out float radius) {
-            var center = capsule.transform.TransformPoint(capsule.center);
-            radius = 0f;
-            var height = 0f;
-            var lossyScale = capsule.transform.lossyScale;
-            lossyScale = new Vector3(Mathf.Abs(lossyScale.x), Mathf.Abs(lossyScale.y), Mathf.Abs(lossyScale.z));
-            var dir = Vector3.zero;
 
-            switch(capsule.direction) {
-                case 0: // x
-                    radius = Mathf.Max(lossyScale.y, lossyScale.z) * capsule.radius;
-                    height = lossyScale.x * capsule.height;
-                    dir = capsule.transform.TransformDirection(Vector3.right);
-                    break;
-                case 1: // y
-                    radius = Mathf.Max(lossyScale.x, lossyScale.z) * capsule.radius;
-                    height = lossyScale.y * capsule.height;
-                    dir = capsule.transform.TransformDirection(Vector3.up);
-                    break;
-                case 2: // z
-                    radius = Mathf.Max(lossyScale.x, lossyScale.y) * capsule.radius;
-                    height = lossyScale.z * capsule.height;
-                    dir = capsule.transform.TransformDirection(Vector3.forward);
-                    break;
-            }
-
-            if(height < radius * 2f) {
-                dir = Vector3.zero;
-            }
-
-            point0 = center + dir * (height * 0.5f - radius);
-            point1 = center - dir * (height * 0.5f - radius);
+        private static void GetWorldSpaceSphere(SphereCollider sphere, out Vector3 center, out float radius) {
+            center = sphere.transform.TransformPoint(sphere.center);
+            radius = sphere.radius;
         }
-
-        // private void ClampScaleFactor(in MiniatureModel WIM) {
-        //     if (ScalingConfig) config.ScaleFactor =
-        //         Mathf.Clamp(config.ScaleFactor, ScalingConfig.MinScaleFactor, ScalingConfig.MaxScaleFactor);
-        // }
         
         private void DetectGrabButtons() {
             var controllers = GameObject.FindWithTag("Player").GetComponentsInChildren<XRController>();
