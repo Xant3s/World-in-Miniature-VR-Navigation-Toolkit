1d6f13a5522fd7067245baeda5dd366117c559d4
Samuel Truman <contact@samueltruman.com>
2020-10-08 01:34:32 +0200

    Updated manual

--- Manual/build.sh
+++ Manual/build.sh
@@ -2,7 +2,9 @@ echo Joining all files..
 utils/join.bat
 echo Building PDF...
 # pandoc content/*.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
-pandoc content/temp.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
+# pandoc content/temp.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
+pandoc content/*.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
+ls
 rm ../Assets/WIMVR/Manual.pdf
 rm ../Assets/WIMVR/Manual.pdf.meta
 cp Manual.pdf ../Assets/WIMVR/Manual.pdf
