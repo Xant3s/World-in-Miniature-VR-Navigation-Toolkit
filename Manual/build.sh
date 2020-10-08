70ed243ff6b0f02c8abe8587454da3b727c901f3
Samuel Truman <contact@samueltruman.com>
2020-10-08 19:05:30 +0200

    .

--- Manual/build.sh
+++ Manual/build.sh
@@ -2,7 +2,7 @@ echo Building PDF...
 # pandoc content/*.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
 # pandoc content/temp.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf
 pandoc content/*.md --toc --include-in-header header.tex -N --template eisvogel.tex -s --from markdown --highlight-style tango --variable urlcolor=cyan -o Manual.pdf && cp Manual.pdf ../Assets/WIMVR/Manual.pdf
-# rm ../Assets/WIMVR/Manual.pdf
-# rm ../Assets/WIMVR/Manual.pdf.meta
+rm ../Assets/WIMVR/Manual.pdf
+rm ../Assets/WIMVR/Manual.pdf.meta
 cp Manual.pdf ../Assets/WIMVR/Manual.pdf
 echo Finished.
