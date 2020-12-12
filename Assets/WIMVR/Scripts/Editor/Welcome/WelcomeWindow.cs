d31f9f5025a6d0cba408f44d6e9ae32a498ef995
Samuel Truman <contact@samueltruman.com>
2020-12-12 13:05:16 +0100

    Improved icon buttons, refactoring

--- Assets/WIMVR/Scripts/Editor/Welcome/WelcomeWindow.cs
+++ Assets/WIMVR/Scripts/Editor/Welcome/WelcomeWindow.cs
@@ -11,46 +11,78 @@ namespace WIMVR.Editor.Welcome {
     /// The welcome window visible at the first start.
     /// </summary>
     public class WelcomeWindow : EditorWindow {
+        private VisualElement root;
+
         [MenuItem("Window/WIMVR/Welcome Window")]
         public static void ShowWindow() {
             var window = GetWindow(typeof(WelcomeWindow), true, "wimVR Welcome");
-            window.minSize = window.maxSize = new Vector2(570, 520);
+            window.minSize = window.maxSize = new Vector2(600, 520);
         }
 
         public void OnEnable() {
-            var root = rootVisualElement;
-            var visualTree = AssetUtils.LoadAtRelativePath<VisualTreeAsset>("WelcomeWindow.uxml", this);
-            var styleSheet = AssetUtils.LoadAtRelativePath<StyleSheet>("WelcomeWindow.uss", this);
-            VisualElement uxmlContents = visualTree.CloneTree();
-            uxmlContents.styleSheets.Add(styleSheet);
-            root.Add(uxmlContents);
+            root = rootVisualElement;
+            LoadHierarchy();
+            LoadStyle();
+            LoadButtonIcons();
+            AddStyleToIconButtons();
+            RegisterOpenExampleSceneEvent();
+            RegisterOpenManualEvent();
+            RegisterSupportMailEvent();
+            ShowOnStartupToggleBehavior();
+        }
 
-            root.Q<Image>(name: "SceneIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_app_dark");
-            root.Q<Image>(name: "ManualIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_manual_dark");
-            root.Q<Image>(name: "EmailIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_mail_dark");
-            root.Q<Image>(name: "RTF-icon").image = Resources.Load<Texture>("RTF/Logo/RTF-Logo-BrightCast");
+        private void ShowOnStartupToggleBehavior() {
+            var showOnStartupToggle = root.Q<Toggle>("showOnStartup");
+            showOnStartupToggle.value = EditorPrefs.GetBool("WIM_Plugin_ShowWelcomeWindowOnStartup", true);
+            showOnStartupToggle.RegisterCallback<ChangeEvent<bool>>(e
+                => EditorPrefs.SetBool("WIM_Plugin_ShowWelcomeWindowOnStartup", e.newValue));
+        }
 
-            root.Q<Button>("ExampleSceneBtn").RegisterCallback<MouseUpEvent>((e) => {
-                var scenePath =
-                    AssetUtils.GetPathRelativeTo("../../../Examples/SimpleExample/SimpleExample.unity", this);
-                EditorSceneManager.OpenScene(scenePath);
+        private void RegisterSupportMailEvent() {
+            root.Q<Button>("SupportEmailBtn").RegisterCallback<MouseUpEvent>(e => {
+                Application.OpenURL("mailto:contact@samueltruman.com");
             });
+        }
 
+        private void RegisterOpenManualEvent() {
             root.Q<Button>("ManualBtn").RegisterCallback<MouseUpEvent>(e => {
                 var manualPath = AssetUtils.GetPathRelativeTo("../../../Manual.pdf", this);
                 manualPath = $"{Application.dataPath}{manualPath.Remove(0, 6)}";
                 Application.OpenURL(manualPath);
             });
+        }
 
-            root.Q<Button>("SupportEmailBtn").RegisterCallback<MouseUpEvent>(e => {
-                Application.OpenURL("mailto:contact@samueltruman.com");
+        private void RegisterOpenExampleSceneEvent() {
+            root.Q<Button>("ExampleSceneBtn").RegisterCallback<MouseUpEvent>(e => {
+                var scenePath =
+                    AssetUtils.GetPathRelativeTo("../../../Examples/SimpleExample/SimpleExample.unity", this);
+                EditorSceneManager.OpenScene(scenePath);
             });
+        }
 
-            root.Q<Toggle>("showOnStartup").value = EditorPrefs.GetBool("WIM_Plugin_ShowWelcomeWindowOnStartup", true);
-            var showOnStartupToggle = root.Q<Toggle>("showOnStartup");
-            showOnStartupToggle.RegisterCallback<ChangeEvent<bool>>((e) => {
-                EditorPrefs.SetBool("WIM_Plugin_ShowWelcomeWindowOnStartup", e.newValue);
+        private void AddStyleToIconButtons() {
+            root.Query(className: "round-button").ForEach(roundButton => {
+                var lightGray = new Color(192 / 255f, 192 / 255f, 192 / 255f);
+                roundButton.RegisterCallback<MouseOverEvent>(e => roundButton.style.backgroundColor = lightGray);
+                roundButton.RegisterCallback<MouseOutEvent>(e => roundButton.style.backgroundColor = Color.white);
             });
+        }
+
+        private void LoadButtonIcons() {
+            root.Q<Image>(name: "ManualIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_manual_dark");
+            root.Q<Image>(name: "EmailIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_mail_dark");
+            root.Q<Image>(name: "RTF-icon").image = Resources.Load<Texture>("RTF/Logo/RTF-Logo-BrightCast");
+            root.Q<Image>(name: "SceneIcon").image = Resources.Load<Texture>("RTF/Icons/RTF_icon_app_dark");
+        }
+
+        private void LoadStyle() {
+            var styleSheet = AssetUtils.LoadAtRelativePath<StyleSheet>("WelcomeWindow.uss", this);
+            root.styleSheets.Add(styleSheet);
+        }
+
+        private void LoadHierarchy() {
+            var visualTree = AssetUtils.LoadAtRelativePath<VisualTreeAsset>("WelcomeWindow.uxml", this);
+            visualTree.CloneTree(root);
         }
     }
 }
