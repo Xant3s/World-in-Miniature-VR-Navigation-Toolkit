7d98c4d3659e97c227c06bf72e666b12ebcc76f8
Samuel Truman <contact@samueltruman.com>
2021-06-03 18:45:09 +0200

    Added hand setup window

---
+++ Assets/WIMVR/Core/VR/Hand Setup/HandSetupWindow.cs
@@ -0,0 +1,27 @@
+// Author: Samuel Truman (contact@samueltruman.com)
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class HandSetupWindow : EditorWindow {
+    private VisualElement root;
+    private GameObject leftHandPrefab;
+    private GameObject rightHandPrefab;
+    
+    
+    [MenuItem("Window/wimVR/Hand Setup")]
+    public static void ShowExample() {
+        var window = GetWindow<HandSetupWindow>();
+        window.titleContent = new GUIContent("wimVR Hand Setup");
+    }
+
+    public void OnEnable() {
+        root = rootVisualElement;
+        var visualTree = Resources.Load<VisualTreeAsset>("HandSetupWindow");
+        visualTree.CloneTree(root);
+        var style = Resources.Load<StyleSheet>("HandSetupWindowStyle");
+        root.styleSheets.Add(style);
+    }
+}
