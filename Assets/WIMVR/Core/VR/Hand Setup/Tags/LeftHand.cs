8d36a2c7f462c567ad45fb7248f2ffb4b07fa95f
Samuel Truman <contact@samueltruman.com>
2021-06-07 22:37:42 +0200

    Added tags for hands

---
+++ Assets/WIMVR/Core/VR/Hand Setup/Tags/LeftHand.cs
@@ -0,0 +1,8 @@
+﻿// Author: Samuel Truman (contact@samueltruman.com)
+
+using UnityEngine;
+
+namespace WIMVR.VR.HandSetup.Tags {
+    [DisallowMultipleComponent]
+    public class LeftHand : MonoBehaviour { }
+}
