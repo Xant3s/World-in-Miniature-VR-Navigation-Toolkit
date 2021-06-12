6235b5efda8655dc0aa2fcdd00c28cf690f311fb
Samuel Truman <contact@samueltruman.com>
2021-06-12 21:38:26 +0200

    Fixed scrolling bugs (scrolling is now a runtime only feature)

--- Assets/WIMVR/Features/Scrolling/Scrolling.cs
+++ Assets/WIMVR/Features/Scrolling/Scrolling.cs
@@ -13,112 +13,33 @@ namespace WIMVR.Features.Scrolling {
     /// <summary>
     /// Allows to scroll the visible part of the miniature model at runtime.
     /// </summary>
-    [ExecuteAlways]
     [DisallowMultipleComponent]
+    [RequireComponent(typeof(WIMInput))]
     public class Scrolling : MonoBehaviour {
         [HideInInspector] public ScrollingConfiguration ScrollingConfig;
 
+        private MiniatureModel WIM;
         private WIMData data;
         private WIMInput wimInput;
         private Vector2 verticalAxisInput;
 
 
-        private void OnEnable() {
-            if(!ScrollingConfig) return;
-            Setup();
-        }
-
-        private void OnDisable() {
-            Remove();
-        }
-
         private void Awake() {
+            WIM = FindObjectOfType<MiniatureModel>();
             wimInput = GetComponent<WIMInput>();
             wimInput.scrollWIM.action.performed += ctx => OnScrollWIM(ctx.action.ReadValue<Vector2>());
             wimInput.scrollWIMVertically.action.performed += ctx => OnScrollWIMVertically(ctx.action.ReadValue<Vector2>());
         }
 
-        private void Setup() {
-            WIMGenerator.OnPreConfigure += DisableScrolling;
-            WIMGenerator.OnConfigure += EnableScrolling;
-            WIMGenerator.OnConfigure += UpdateScrollingMask;
+        private void Start() {
+            if(!ScrollingConfig || !ScrollingConfig.AllowWIMScrolling) return;
+            EnableScrolling(WIM);
+            UpdateScrollingMask(WIM);
             PlayerRepresentation.OnUpdatePlayerRepresentationInWIM += AdjustPlayerRepresentationInWIM;
-            if (!Application.isPlaying) return;
             MiniatureModel.OnUpdate += ScrollWIM;
         }
-
-        private void Remove() {
-            WIMGenerator.OnPreConfigure -= DisableScrolling;
-            WIMGenerator.OnConfigure -= EnableScrolling;
-            WIMGenerator.OnConfigure -= UpdateScrollingMask;
-            PlayerRepresentation.OnUpdatePlayerRepresentationInWIM -= AdjustPlayerRepresentationInWIM;
-            if (!Application.isPlaying) return;
-            MiniatureModel.OnUpdate -= ScrollWIM;
-        }
-
-        private void OnDestroy() {
-            var WIM = FindObjectOfType<MiniatureModel>();
-            if(!WIM) return;
-            DisableScrolling(WIM);
-            WIMGenerator.SetWIMMaterial(WIMGenerator.LoadDefaultMaterial(WIM), WIM);
-        }
-
-        private void UpdateScrollingMask(in MiniatureModel WIM) {
-            if (!ScrollingConfig || !ScrollingConfig.AllowWIMScrolling) return;
-            var boxMaskObj = FindObjectOfType<BoxMask>().gameObject;
-            if (!boxMaskObj) return;
-            boxMaskObj.transform.localScale = WIM.Configuration.ActiveAreaBounds;
-        }
-
-        private static void DisableScrolling(in MiniatureModel WIM) {
-            var boxMask = FindObjectOfType<BoxMask>()?.gameObject;
-#if UNITY_EDITOR
-            if (boxMask) Undo.DestroyObjectImmediate(boxMask);
-#else
-            GameObject.DestroyImmediate(boxMask);
-#endif
-            WIM.transform.RemoveAllColliders();
-            WIMGenerator.GenerateColliders(WIM);
-        }
-
-
-        private void ScrollWIM(WIMConfiguration config, WIMData data) {
-            Assert.IsNotNull(ScrollingConfig, "Scrolling configuration is missing.");
-            this.data = data;
-            if (!data.WIMLevelTransform) return;    // TODO: Useless?
-            if(!ScrollingConfig.AllowWIMScrolling) return;
-            if (ScrollingConfig.AutoScroll) AutoScrollWIM();
-        }
-
-        private void OnScrollWIM(Vector2 value) => ManuallyScrollWIM(value);
-
-        private void OnScrollWIMVertically(Vector2 value) {
-            verticalAxisInput = value;
-            ManuallyScrollWIM(Vector3.zero);
-        }
-
-        private void ManuallyScrollWIM(Vector3 scrollingInput) {
-            if (!Application.isPlaying) return;
-            if (!ScrollingConfig.AllowWIMScrolling) return;
-            var direction = new Vector3(scrollingInput.x, verticalAxisInput.y, scrollingInput.y);
-            if(!ScrollingConfig.AllowVerticalScrolling) direction.y = 0;
-            Assert.IsNotNull(data.WIMLevelTransform);
-            data.WIMLevelTransform.Translate(Time.deltaTime * ScrollingConfig.ScrollSpeed * -direction, Space.World);
-        }
-
-        private void AutoScrollWIM() {
-            if (!ScrollingConfig.AllowWIMScrolling || !ScrollingConfig.AutoScroll ||
-                !data.PlayerRepresentationTransform) return;
-            Assert.IsNotNull(data.PlayerRepresentationTransform);
-            Assert.IsNotNull(data.WIMLevelTransform);
-            var scrollOffset = data.DestinationIndicatorInWIM
-                ? -data.DestinationIndicatorInWIM.localPosition
-                : -data.PlayerRepresentationTransform.localPosition;
-            data.WIMLevelTransform.localPosition = scrollOffset;
-        }
-
-        private void EnableScrolling(in MiniatureModel WIM) {
-            if (!ScrollingConfig || !ScrollingConfig.AllowWIMScrolling) return;
+        
+        private static void EnableScrolling(in MiniatureModel WIM) {
             var maskController = new GameObject("Box Mask");
 #if UNITY_EDITOR
             Undo.RegisterCreatedObjectUndo(maskController, "Created Box Mask");
@@ -133,6 +54,64 @@ namespace WIMVR.Features.Scrolling {
             WIM.gameObject.AddComponent<BoxCollider>().size =
                 WIM.Configuration.ActiveAreaBounds / WIM.Configuration.ScaleFactor;
             maskController.transform.position = WIM.transform.position;
+        }
+
+        public void DisableScrolling() {
+            UnregisterCallbacks();
+            RemoveBoxMask();
+            WIM.transform.RemoveAllColliders();
+            WIMGenerator.GenerateColliders(WIM);
+        }
+
+        private void UnregisterCallbacks() {
+            PlayerRepresentation.OnUpdatePlayerRepresentationInWIM -= AdjustPlayerRepresentationInWIM;
+            MiniatureModel.OnUpdate -= ScrollWIM;
+        }
+
+        private static void RemoveBoxMask() {
+            var boxMask = FindObjectOfType<BoxMask>()?.gameObject;
+#if UNITY_EDITOR
+            if(boxMask) Undo.DestroyObjectImmediate(boxMask);
+#else
+            GameObject.DestroyImmediate(boxMask);
+#endif
+        }
+
+        private void OnScrollWIM(Vector2 value) => ManuallyScrollWIM(value);
+
+        private void OnScrollWIMVertically(Vector2 value) {
+            verticalAxisInput = value;
+            ManuallyScrollWIM(Vector3.zero);
+        }
+
+        private void ManuallyScrollWIM(Vector3 scrollingInput) {
+            if(!ScrollingConfig || !ScrollingConfig.AllowWIMScrolling) return;
+            var direction = new Vector3(scrollingInput.x, verticalAxisInput.y, scrollingInput.y);
+            if(!ScrollingConfig.AllowVerticalScrolling) direction.y = 0;
+            Assert.IsNotNull(data.WIMLevelTransform);
+            data.WIMLevelTransform.Translate(Time.deltaTime * ScrollingConfig.ScrollSpeed * -direction, Space.World);
+        }
+
+        private void ScrollWIM(WIMConfiguration wimConfig, WIMData wimData) {
+            data = wimData;
+            if(!wimData.WIMLevelTransform) return;
+            if(ScrollingConfig.AutoScroll) AutoScrollWIM();
+        }
+
+        private void AutoScrollWIM() {
+            if(!data.PlayerRepresentationTransform) return;
+            Assert.IsNotNull(data.PlayerRepresentationTransform);
+            Assert.IsNotNull(data.WIMLevelTransform);
+            var scrollOffset = data.DestinationIndicatorInWIM
+                ? -data.DestinationIndicatorInWIM.localPosition
+                : -data.PlayerRepresentationTransform.localPosition;
+            data.WIMLevelTransform.localPosition = scrollOffset;
+        }
+
+        private static void UpdateScrollingMask(in MiniatureModel WIM) {
+            var boxMaskObj = FindObjectOfType<BoxMask>().gameObject;
+            if(!boxMaskObj) return;
+            boxMaskObj.transform.localScale = WIM.Configuration.ActiveAreaBounds;
         }
 
         private void AdjustPlayerRepresentationInWIM(WIMConfiguration config, WIMData data) {
